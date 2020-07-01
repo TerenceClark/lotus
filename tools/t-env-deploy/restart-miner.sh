640df0725b9ad9432d33e648ee46f79844d682e0
刘林欣 <llx@ipfsmain.cn>
2020-07-01 19:59:06 +0800

    add sh

--- tools/t-env-deploy/restart-miner.sh
+++ tools/t-env-deploy/restart-miner.sh
@@ -1,3 +1,5 @@
+source export-env.sh
 ps aux|grep lotus|awk '{print $2}'|xargs kill -9
+sleep 3
 nohup ./lotus-storage-miner run > lotus.log &
 
--- tools/t-env-deploy/restart-node.sh
+++ tools/t-env-deploy/restart-node.sh
@@ -1,2 +1,4 @@
+source export-env.sh
 ./lotus daemon stop
+sleep 3
 nohup ./lotus daemon > lotus.log &
